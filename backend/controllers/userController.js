fbfc270a825a074855c1b2c6ce7162b209690503
Javed Chauhan <chauhanjaved@gmail.com>
2023-08-07 11:50:43 +0530

    Update in user related endpoints
    - Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,15 @@
-const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+const generateToken = (userId) => {
+    return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
 };
 
-const registerUser = async (req, res) => {
+// User Registration
+const registerUser = async (req, res) => {    
     const { name, email, password } = req.body;
 
     const userExists = await User.findOne({ email });
@@ -31,48 +34,57 @@ const registerUser = async (req, res) => {
     res.status(201).json({ message: 'User registered successfully', token });
 };
 
+// User Login
 const loginUser = async (req, res) => {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
 
-    if(!user) {
-        return res.status(404).json({ message: 'User not found' });
+    if (!user) {
+        return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-    const isMatch = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
-    if(!isMatch) {
-        return res.status(400).json({ message: 'Invalid credentials' });
+    if (!isPasswordValid) {
+        return res.status(400).json({ message: 'Invalid email or password' });
     }
 
     const token = generateToken(user._id);
 
-    res.json({ message: 'User logged in successfully', token });
+    res.json({ message: 'Logged in successfully', token });
 };
 
+// Get user profile
 const getUserProfile = async (req, res) => {
-    res.json(req.user);
+    const user = await User.findById(req.user.id);
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
 };
 
+// Update user profile
 const updateUserProfile = async (req, res) => {
-    const { name, email, password } = req.body;
+    const user = await User.findById(req.user.id);
 
-    const user = req.user;
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
 
-    user.name = name || user.name;
-    user.email = email || user.email;
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
 
-    if(password) {
+    if (req.body.password) {
         const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
-        user.password = hashedPassword;
+        user.password = await bcrypt.hash(req.body.password, salt);
     }
 
     const updatedUser = await user.save();
 
-    res.json({ message: 'User updated', user: updatedUser });
+    res.json({ message: 'Profile updated', user: updatedUser });
 };
 
 module.exports = { registerUser, loginUser, getUserProfile, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,9 +3,16 @@ const router = express.Router();
 const { registerUser, loginUser, getUserProfile, updateUserProfile } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
+// POST /api/users/register
 router.post('/register', registerUser);
+
+// POST /api/users/login
 router.post('/login', loginUser);
+
+// GET /api/users/profile
 router.get('/profile', auth, getUserProfile);
+
+// PUT /api/users/profile
 router.put('/profile', auth, updateUserProfile);
 
 module.exports = router;
